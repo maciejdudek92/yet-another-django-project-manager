9fd91bcdaaaa328b35b30c8ab7441672bbf30f22
maciejdudek92 <maciej.dudek@hotmail.com>
2024-10-21 14:58:00 +0200

    update

--- build.py
+++ build.py
@@ -14,14 +14,6 @@ def prepare_assets():
 
     shutil.make_archive(zip_file, "zip", folder)
 
-    # with zipfile.ZipFile(zip_file, "w") as _zip:
-    #     for folder_name, subfolders, filenames in os.walk(folder):
-    #         for filename in filenames:
-    #             # Create complete filepath of file in directory
-    #             file_path = os.path.join(folder_name, filename)
-    #             # Add file to zip
-    #             _zip.write(file_path, filename)
-
 
 def build() -> None:
     prepare_assets()
